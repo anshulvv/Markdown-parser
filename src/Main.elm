fc1f0582f157afe20001fc39010053b31d229d69
anshulvv <thisisanshulverma@gmail.com>
2024-09-14 13:24:47 +0900

    Added markdown parser and kind of made it working

--- src/Main.elm
+++ src/Main.elm
@@ -1,16 +1,25 @@
 module Main exposing (main)
 
 import Browser
+import Element exposing (Element)
+import Element.Background
+import Element.Border
+import Element.Font as Font
+import Element.Input
 import Html exposing (Html)
+import Html.Attributes
+import Markdown.Block exposing (ListItem(..), Task(..))
+import Markdown.Html
+import Markdown.Parser
+import Markdown.Renderer
 
 
 type alias Model =
-    { count : Int }
+    String
 
 
 type Msg
-    = Increment
-    | Decrement
+    = OnMarkdownChange String
 
 
 main : Program () Model Msg
@@ -24,19 +33,200 @@ main =
 
 
 init flags =
-    ( { count = 0 }, Cmd.none )
+    ( "", Cmd.none )
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        OnMarkdownChange markdown ->
+            ( markdown, Cmd.none )
 
 
 view : Model -> Browser.Document Msg
 view model =
     { title = "Title of the page"
-    , body = []
+    , body =
+        [ Element.layout []
+            (Element.column [ Element.width Element.fill ]
+                [ Element.Input.multiline
+                    []
+                    { onChange = OnMarkdownChange
+                    , text = model
+                    , placeholder = Nothing
+                    , label = Element.Input.labelHidden "Markdown Editor"
+                    , spellcheck = False
+                    }
+                , case viewMarkdown model of
+                    Ok rendered ->
+                        Element.column
+                            []
+                            rendered
+
+                    Err err ->
+                        Element.text <| err
+                ]
+            )
+        ]
     }
+
+
+viewMarkdown model =
+    model
+        |> Markdown.Parser.parse
+        |> Result.mapError (List.map Markdown.Parser.deadEndToString >> String.join "\n")
+        |> Result.andThen (\ast -> Markdown.Renderer.render renderer ast)
+
+
+renderer : Markdown.Renderer.Renderer (Element Msg)
+renderer =
+    elmUiRenderer
+
+
+elmUiRenderer : Markdown.Renderer.Renderer (Element Msg)
+elmUiRenderer =
+    { heading = heading
+    , paragraph =
+        Element.paragraph
+            [ Element.spacing 15 ]
+    , thematicBreak = Element.none
+    , text = Element.text
+    , strong = \content -> Element.row [ Font.bold ] content
+    , emphasis = \content -> Element.row [ Font.italic ] content
+    , strikethrough = \content -> Element.row [ Font.strike ] content
+    , codeSpan = code
+    , link =
+        \{ title, destination } body ->
+            Element.newTabLink
+                [ Element.htmlAttribute (Html.Attributes.style "display" "inline-flex") ]
+                { url = destination
+                , label =
+                    Element.paragraph
+                        [ Font.color (Element.rgb255 0 0 255)
+                        ]
+                        body
+                }
+    , hardLineBreak = Html.br [] [] |> Element.html
+    , image =
+        \image ->
+            case image.title of
+                Just title ->
+                    Element.image [ Element.width Element.fill ] { src = image.src, description = image.alt }
+
+                Nothing ->
+                    Element.image [ Element.width Element.fill ] { src = image.src, description = image.alt }
+    , blockQuote =
+        \children ->
+            Element.column
+                [ Element.Border.widthEach { top = 0, right = 0, bottom = 0, left = 10 }
+                , Element.padding 10
+                , Element.Border.color (Element.rgb255 145 145 145)
+                , Element.Background.color (Element.rgb255 245 245 245)
+                ]
+                children
+    , unorderedList =
+        \items ->
+            Element.column [ Element.spacing 15 ]
+                (items
+                    |> List.map
+                        (\(ListItem task children) ->
+                            Element.row [ Element.spacing 5 ]
+                                [ Element.row
+                                    [ Element.alignTop ]
+                                    ((case task of
+                                        IncompleteTask ->
+                                            Element.Input.defaultCheckbox False
+
+                                        CompletedTask ->
+                                            Element.Input.defaultCheckbox True
+
+                                        NoTask ->
+                                            Element.text "•"
+                                     )
+                                        :: Element.text " "
+                                        :: children
+                                    )
+                                ]
+                        )
+                )
+    , orderedList =
+        \startingIndex items ->
+            Element.column [ Element.spacing 15 ]
+                (items
+                    |> List.indexedMap
+                        (\index itemBlocks ->
+                            Element.row [ Element.spacing 5 ]
+                                [ Element.row [ Element.alignTop ]
+                                    (Element.text (String.fromInt (index + startingIndex) ++ " ") :: itemBlocks)
+                                ]
+                        )
+                )
+    , codeBlock = codeBlock
+    , html = Markdown.Html.oneOf []
+    , table = Element.column []
+    , tableHeader = Element.column []
+    , tableBody = Element.column []
+    , tableRow = Element.row []
+    , tableHeaderCell =
+        \maybeAlignment children ->
+            Element.paragraph [] children
+    , tableCell =
+        \maybeAlignment children ->
+            Element.paragraph [] children
+    }
+
+
+heading :
+    { level : Markdown.Block.HeadingLevel
+    , rawText : String
+    , children : List (Element Msg)
+    }
+    -> Element Msg
+heading { level, rawText, children } =
+    Element.column
+        [ Font.size
+            (case level of
+                Markdown.Block.H1 ->
+                    30
+
+                _ ->
+                    20
+            )
+        ]
+        children
+
+
+code : String -> Element msg
+code snippet =
+    Element.el
+        [ Element.Background.color
+            (Element.rgba 0 0 0 0.04)
+        , Element.Border.rounded 2
+        , Element.paddingXY 5 3
+        , Font.family
+            [ Font.external
+                { url = "https://fonts.googleapis.com/css?family=Source+Code+Pro"
+                , name = "Source Code Pro"
+                }
+            ]
+        ]
+        (Element.text snippet)
+
+
+codeBlock : { body : String, language : Maybe String } -> Element msg
+codeBlock details =
+    Element.el
+        [ Element.Background.color (Element.rgba 0 0 0 0.03)
+        , Element.htmlAttribute (Html.Attributes.style "white-space" "pre")
+        , Element.padding 20
+        , Font.family
+            [ Font.external
+                { url = "https://fonts.googleapis.com/css?family=Source+Code+Pro"
+                , name = "Source Code Pro"
+                }
+            ]
+        ]
+        (Element.text details.body)
 
 
 subscriptions : Model -> Sub Msg
